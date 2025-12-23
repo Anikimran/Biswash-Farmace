8c02f66b9a1de5959b374c3474159230cbe78152
MD.Anik Imran <mdanikimran17@gmail.com>
2025-12-23 14:43:22 +0600

    update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,13 +4,69 @@ import { useState } from "react";
 import { Search, X } from "lucide-react";
 
 const data = [
-  " আজিজ সরদার,স-১। স ।৳৩০০০ ",
-  "আবুল হোসেন ,স",
-  "MacBook Air",
-  "MacBook Pro",
-  "iPad Pro",
-  "Apple Watch",
-  "AirPods Pro",
+  " ১।আজিজ সরদার -শ্রীকলা -৳৪৭৫ ",
+  " ২।আবু হোসেন। আজিজ সরদার। শৃকলা -৳৫৯২৮ ",
+  " ৩।রফিকুল সদ্দার। শৃকলা-৳১২০৫২ ",
+  " ৪।বাবলু মাস্টার। আব্দুল কাদের। শৃঙ্খলা -৳১৬৫৭ ",
+  " ৫।রুবেল কম্পিউটার। ৳-৩৪৯৩ ",
+  " ৬।কমলেশ। বেলি পুকুর। ৳-৫৭০ ",
+  " ৭।হেলাল হুজুর। ৳-৭৯০ ",
+  "৮।মিজানুর স্যার। রজব আলী। -৳-২৬৮৪  ",
+  " ৯।আইয়ুব ভাই। ৳-১৩২০ ",
+  " ১০।লিটন গাজী। মালেক গাজী। -৳১৫০ ",
+  "  ১১।কুদ্দুস সরদার। তমিজ উদ্দিন। ৳১৮৩০",
+  "১২।শাকিলা ভাবি। ৳-১৬০৭  ",
+  " ১৩।রবিউল ঢালী। নেসার ঢালী। ৳-১২৬০ ",
+  " ১৪।শরিফুল হার্ডওয়ার্ক।-৳-৩৯৬ ",
+  " ১৫।সাইন সরদার। আবুল.-৳১৫৬৩ ",
+  " ১৬।বাবু মল্লিক। নিরদ মল্লিক ৳৮০ ",
+  "১৭.আমিন সরদার। মোকসে সদ্দার।-৳-৩৫০  ",
+  "১৮.মমতাজ স্যার ৳-১৯২০  ",
+  "২০।হাকিম ইলেকট্রনিক। ৳-১২৪৫  ",
+  "২১.সিদ্দিক ভাই ১৬১২ টাকা  ",
+  " ২২.রাজ্জাক ভাই মাছ ৳-১৮৮ ",
+  " ২৪.সবুর সরদার ৭০৪৮ টাকা ",
+  " ২৫.ডাক্তার হাকিম ১৭০ টাকা ",
+  " ২৬.শফিকুল চাচা ১৫১৩ টাকা ",
+  " ২৭.আবেদার বোনাই ২৩৮৮৩ টাকা ",
+  " ২৯.মাসুম চা-৳-২৯৩৪ ",
+  " ৩১.ডাক্তার লেন্টু-৳-৫১৮ ",
+  " ৩২.শাহাজামাল চা ৳-৩৬৮২ ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
+  "  ",
 ];
 
 export default function Page() {
@@ -23,13 +79,18 @@ export default function Page() {
   return (
     <div className="w-full max-w-xl mx-auto mt-20 px-4 bg-red-200 text-black shadow-2xl rounded-xl px-5 py-5 md:px-8 py-8">
       {/* Search Bar */}
-      <h1 className="text-bold text-2xl text-red-600">All Costomer List</h1>
+      <h1 className="text-2xl text-green-600 font-bold text-center">
+        Biswash Farmace
+      </h1>
+      <h1 className="text-bold text-2xl text-red-600 mt-2">
+        All Costomer List
+      </h1>
       <div className="flex items-center gap-3 bg-gray-100 rounded-full px-5 py-3 shadow-sm mt-5 ">
         <Search className="text-gray-500 p-5" size={18} />
         <input
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search Apple.com"
+          placeholder="Search by Costomer Name"
           className="bg-transparent w-full outline-none text-gray-700 placeholder-gray-500"
         />
         {query && (
